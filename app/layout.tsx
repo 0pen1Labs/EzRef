7128ac4c599ae5e922cf6e224bdcb272f4410131
leocoder <singhra66@gmail.com>
2023-11-05 15:34:43 +0530

    feat: added dark and light theme support issue #2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { JetBrains_Mono } from 'next/font/google'
 import './globals.css'
+import { Providers } from './providers'
 
 const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'] })
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={jetBrainsMono.className}>{children}</body>
+      <body className={jetBrainsMono.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }

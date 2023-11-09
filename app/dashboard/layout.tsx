a24b4274772fcfc7eda0ece2609f50fd9bb4c5da
leocoder <singhra66@gmail.com>
2023-11-09 21:52:47 +0530

    fix: fixed nav style

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,11 +8,11 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex h-screen w-screen flex-row">
-      <div className="flex w-52 border-r border-foreground/50">
+      <div className="flex w-52 border-r border-foreground/20">
         <DashboardSideNav />
       </div>
       <div className="flex flex-grow flex-col">
-        <div className="flex justify-end border-b border-foreground/50 px-10 py-4 drop-shadow-md">
+        <div className="flex justify-end border-b border-foreground/20 px-10 py-4 drop-shadow-md">
           <DashboardTopNav />
         </div>
         <div className="h-screen">{children}</div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,18 +8,25 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header className="bg-background px-32 py-8 text-foreground">
+    <header className="bg-background px-32 py-4 pt-6 text-foreground">
       <nav className="flex items-center justify-between">
         <Logo />
         {/* TODO add Login and sign up flow hare */}
-        <ul className="flex items-center gap-4">
+        <ul className="flex items-center space-x-3">
           <li className="rounded-md bg-purple/50 px-8 py-2 text-sm font-light hover:cursor-pointer hover:bg-purple/80">
-            <Link href={'/dashboard'}>Dashboard</Link>
+            <Link className="text-sm text-foreground" href={'/dashboard'}>
+              Dashboard
+            </Link>
           </li>
-          <li className="text-sm font-light hover:opacity-70">
-            <Link href={'/dashboard'}>Signup</Link>
+          <li className="hover:opacity-70">
+            <Link
+              className="text-sm font-light text-foreground hover:opacity-70"
+              href={'/dashboard'}
+            >
+              Signup
+            </Link>
           </li>
-          <li className="hover:opacity-80">
+          <li className="flex items-center justify-center hover:opacity-80">
             <Link href={'https://github.com/0pen1Labs/RefLink'} isExternal>
               <GithubButton />
             </Link>

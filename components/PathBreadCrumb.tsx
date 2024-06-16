c8f98323eea16fb7177460874e85d53ec1f3b1b0
leocoder <singhra66@gmail.com>
2024-06-16 21:09:17 +0530

    fix: breadcrumb fixed

--- components/PathBreadCrumb.tsx
+++ components/PathBreadCrumb.tsx
@@ -1,43 +1,45 @@
 'use client'
 
-import { Breadcrumbs, BreadcrumbItem } from '@nextui-org/react'
 import { usePathname } from 'next/navigation'
 import { HomeIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-import Image from 'next/image'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from './ui/breadcrumb'
 
 function PathBreadCrumb() {
-  const paths = usePathname()
-  const pathNames = paths.split('/').filter((path) => path)
+  const paths = usePathname();
+  console.log("paths", paths)
+  const pathNames = paths.split('/').filter((path) => path).splice(0, 2);
 
-  const handleOnPress = (item: string) => {}
 
   return (
-    <Breadcrumbs className="item-center" underline="active" separator=">">
+    <Breadcrumb className="item-center" separator=">">
+      <BreadcrumbList>
       {pathNames.map((item, index) => {
         let href = `/${pathNames.slice(0, index + 1).join('/')}`
         let pathItem = item[0].toLocaleUpperCase() + item.slice(1, item.length)
         return (
-          <BreadcrumbItem
-            key={item}
-            isCurrent={paths === href}
-            onPress={() => handleOnPress(item)}
-          >
-            <Link href={`/${item}`}>
-              {pathItem.toLocaleLowerCase() === 'dashboard' ? (
-                <HomeIcon
-                  className={`h-4 w-6 text-foreground ${
-                    paths !== href && 'opacity-50'
-                  }`}
-                />
-              ) : (
-                pathItem
-              )}
-            </Link>
-          </BreadcrumbItem>
+          <div key={index} className='flex flex-row items-center'>
+            <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href={`/${item}`}>
+                    {pathItem.toLocaleLowerCase() === 'dashboard' ? (
+                      <HomeIcon
+                      className={`h-4 w-6 text-foreground hover:opacity-100 ${
+                        paths !== href && 'opacity-50'
+                      }`}
+                      />
+                    ) : (
+                      pathItem
+                    )}
+                  </Link>
+                </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className={index === pathNames.length - 1 ? 'hidden' : ''}/>
+          </div>
         )
       })}
-    </Breadcrumbs>
+      </BreadcrumbList>
+    </Breadcrumb>
   )
 }
 

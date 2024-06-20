d4fed08f79702fffcb69646af3d9d326f994e16d
leocoder <singhra66@gmail.com>
2024-06-20 22:01:12 +0530

    feat: added create user in server component.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,43 +1,33 @@
 import GenerateNewLinkCard from '@/components/GenerateNewLinkCard'
 import { currentUser } from '@clerk/nextjs'
 import { User } from '@clerk/nextjs/server'
-import { auth } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
 import HomeLinkList from '@/components/HomeLinkList'
 import { Separator } from '@/components/ui/separator'
 import { SparklesCore } from '@/components/ui/sparkles'
+import { prisma } from '@/lib/db'
 
 async function sendClerkId(user: User) {
-  const { getToken } = auth()
+  const { userId,  } = auth()
   const email = user.emailAddresses[0].emailAddress
-  const token = await getToken()
-  const response = await fetch(`${process.env.BASE_URL}/v1/api/auth/register`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      email: email,
-    }),
-  })
 
-  const userData = await response.json()
-  console.log(userData)
-}
-
-const getLinkList = async () => {
-  const { getToken } = auth()
-  const token = await getToken()
-  const response = await fetch(`${process.env.BASE_URL}/v1/api/ref/links`, {
-    next: {
-      tags: ['links'],
-    },
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  if(userId){
+    const createUser = await prisma.user.upsert({
+      where: {
+        clerkId: userId,
+      },
+      update: {},
+      create: {
+        email,
+        clerkId: userId,
+      },
+      select: {
+        id: true,
+        email: true,
+      },
+    }) 
+    console.log(createUser);
+  }
 }
 export default async function Dashboard() {
   const user = await currentUser()
@@ -78,32 +68,3 @@ export default async function Dashboard() {
     </div>
   )
 }
-
-
-
-<button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
-  <span className="absolute inset-0 overflow-hidden rounded-full">
-    <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-  </span>
-  <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-    <span>
-      Tailwind Connect
-    </span>
-    <svg
-      fill="none"
-      height="16"
-      viewBox="0 0 24 24"
-      width="16"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M10.75 8.75L14.25 12L10.75 15.25"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="1.5"
-      />
-    </svg>
-  </div>
-  <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-</button>
